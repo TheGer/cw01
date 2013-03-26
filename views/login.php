48421e09571447ba3645b6f466a90f11e2a35db0
Gerry Said <gerrysaid@gmail.com>
2013-03-26 21:11:55 +0100

    added register link

--- views/login.php
+++ views/login.php
@@ -4,5 +4,6 @@
     Password: <input type="password" name="password"/>
     <input type="submit" value="login"/>
 </form>
+<p><a href="<?= SITE_ROOT ?>/index.php?page=users&action=register">register with us!</a></p>
 
 
