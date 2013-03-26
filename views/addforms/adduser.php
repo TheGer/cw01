0d417643f4150a9f7a418fa89a3e3295210c982e
Gerry Said <gerrysaid@gmail.com>
2013-03-26 16:56:04 +0100

    fixed jquery and implemented pre validation

--- views/addforms/adduser.php
+++ views/addforms/adduser.php
@@ -6,6 +6,8 @@
 ?>
 
 <div class="twelve columns">
+    <span></span>
+    
     <form id="adduser" method="get" action="<?= SITE_ROOT ?>">
         <input type="hidden" name="page" value="users"/>
         <input type="hidden" name="action" value="add"/>
@@ -14,16 +16,20 @@
         <label class="left inline">Name:</label> 
         <input type="text" name="firstname" />
         <label class="left inline">Password:</label> 
-        <input type="text" name="password1" id="password1" />
+        <input type="text" name="pword1" id="password1" />
         <label class="left inline">Confirm Password:</label> 
-        <input type="text" name="password2" id="password2" />
+        <input type="text" name="pword2" id="password2" />
         <!-- to do add user types -->
         <input type="submit" value="add new user"/>
     </form>
 </div>
 <script>
 $(document).ready(function(){
-alert("archgfds");
+
+$("#adduser").submit(function(){
+    //implement validation here
+    return true;
+});
     
 });
         
