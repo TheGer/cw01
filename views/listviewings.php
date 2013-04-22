5bb36f29d47a96efbbaae694934089449b0a335e
Gerry Said <gerrysaid@gmail.com>
2013-04-22 13:57:58 +0200

    finalizing the edit car function

--- views/listviewings.php
+++ views/listviewings.php
@@ -6,7 +6,7 @@
         ?>
         <tr>     
             <td><?= $viewing->id ?></td>
-            <td><a href="<?= SITE_ROOT ?>/index.php?page=user&action=profile&id=<?= $viewing->userid ?>"><?= $viewing->userid ?></a></td>
+            <td><a href="<?= SITE_ROOT ?>/index.php?page=users&action=profile&id=<?= $viewing->userid ?>"><?= $viewing->userid ?></a></td>
             <td><a href="<?= SITE_ROOT ?>/index.php?page=car&action=showdetails&id=<?= $viewing->carid ?>">View car details</a></td>
             <td><?= date('Y-m-d',$viewing->dateofviewing); ?></td>
             <td><a href="<?= SITE_ROOT ?>/index.php?page=viewing&action=delete&id=<?= $viewing->id ?>" onClick="return confirm('Are you sure?');">Cancel</a></td>
